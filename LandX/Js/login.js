1375744b5eb5184328f0896beb9d76b9ca3fef83
zervoter <zervoter@mail.ru>
2017-03-03 12:33:11 +0200

    Add files via upload

---
+++ LandX/Js/login.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $("#slider").slick({
+        infinite: !0,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        arrows: !1,
+        dots: !0
+    });
+});
